df7abb30ad02c0b2c772ee52b297d168eaae5019
Paulo Bardes <paulobardes@ig.com.br>
2015-06-28 01:43:33 -0300

    Feito: Esqueleto do produto

---
+++ src/br/usp/icmc/mercado/Produto.java
@@ -0,0 +1,16 @@
+package br.usp.icmc.mercado;
+
+import java.util.*;
+
+public class Produto implements Registro
+{
+    public void carregaDados(Stack<String> dados)
+    {
+    }
+
+    public Stack<String> pegaDados()
+    {
+        Stack<String> s = new Stack<String>();
+        return s;
+    }
+}
